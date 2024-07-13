801bef30c1270425a4d20a8973ac1c16e8665871
Maxim Novikov <mn7795807@gmail.com>
2024-07-13 20:42:18 +0700

    Сделал тестовые задачи

---
+++ js/functions.js
@@ -0,0 +1,19 @@
+const lineLength = function (line, maxLength) {
+  if (line <= maxLength) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const reverseLine = function (string) {
+  string = string.replaceAll(' ', '');
+  string = string.toLowerCase();
+  let reversed = '';
+  for (let i = string.length - 1; i >= 0; i--) {
+    reversed = reversed + string[i];
+  }
+  console.log(string === reversed);
+  return string === reversed;
+};
+
